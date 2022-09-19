49ff10bb86febe46ea21a2c3b0dcbc31ef5a1d40
alexandra.dover <alexandra.dover@infinityworks.com>
2022-09-19 14:10:14 +0100

    Added missing files for photograph page.

---
+++ js/photographs.js
@@ -0,0 +1,15 @@
+import { Fancybox } from "https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.esm.js";
+
+Fancybox.bind("[data-fancybox]", {
+  Toolbar: {
+    display: [
+      { id: "counter", position: "center" },
+      { id: "download", position: "right" },
+      { id: "zoom", position: "right" },
+      { id: "slideshow", position: "right" },
+      { id: "fullscreen", position: "right" },
+      { id: "thumbs", position: "right" },
+      { id: "close", position: "right" },
+    ],
+  },
+});
